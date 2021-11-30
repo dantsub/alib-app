4c35bc51be0730a71ac27da02eeceb679fd8620a
nldams <nldams@gmail.com>
2021-11-30 10:30:02 -0500

    Crearequipo

--- src/components/Crearequipo.jsx
+++ src/components/Crearequipo.jsx
@@ -30,10 +30,6 @@ export function Crearequipo(){
                                     <div className="form-group">
                                         <label for="" className="form-label">Nombre del Equipo</label>
                                         <input type="text"  className="form-control" id="doc" placeholder="Nombre del equipo" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label for="" className="form-label">Manager del Equipo</label>
-                                        <input type="text"  className="form-control" id="manager" placeholder="Nombre del manager" />
                                     </div>                                    
                                     <div className="form-group">
                                     <label for="" className="form-label">Logo del Equipo</label>
