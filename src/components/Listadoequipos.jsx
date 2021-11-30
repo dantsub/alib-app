572cd70174d697f5e70e8fd0062976e709c93598
nldams <nldams@gmail.com>
2021-11-30 13:45:59 -0500

    ListadoEquipos

--- src/components/Listadoequipos.jsx
+++ src/components/Listadoequipos.jsx
@@ -2,7 +2,7 @@ import real_madrid from '../asset/real_madrid.jpg';
 import Chelsea_FC from '../asset/Chelsea_FC.jpg';
 import juventus from '../asset/juventus.jpg';
 import barza from '../asset/barza.jpg';
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 export function Listadoequipos (){
     document.title="Listadoequipo";
@@ -44,7 +44,7 @@ export function Listadoequipos (){
                 <i class="fa fa-plus-square"></i> Adicionar equipos
             </button> 
                 
-            {/* <!-- Modal Adicionar Campeonato  --> */}
+            {/* <!-- Modal ADICIONAR EQUIPOS A CAMPEONATOS  --> */}
     
             <div className="modal fade"
                 id="modal_adicionar_campeonato"
@@ -54,21 +54,21 @@ export function Listadoequipos (){
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">
-                            Listado de equipos
-                        </h5>
-                        <button 
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        ></button>
+                            <h5 className="modal-title" id="exampleModalLabel">
+                                Adicionar equipos a campeonatos
+                            </h5>
+                            <button 
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
                         </div>
                         <form
-                        id="adicionarcampeonato"
+                        id="adicionarequiposcampeonato"
                         action=""
                         >
-                        <input type="hidden" name="oculto" value="adicionarjugador"/>
+                        <input type="hidden" name="oculto" value="adicionarequiposcampeonato"/>
                         <input type="hidden" name="ocultoborrar" value=""/>
                         <div className="modal-body">
                             <table className="table table-hover">
@@ -82,28 +82,26 @@ export function Listadoequipos (){
                                 </thead>
 
 
-                                <tbody className="js-table-body" id="tablajugadores">
+                                <tbody className="js-table-body" id="tablaequipos">
                                     {listado_equipos.map(equip=>
                                         <tr>
                                             <td>
                                                 <div className="container">
                                                     <div className="col-md-4 px-0">
-                                                        <img src={equip.logo} className="img-fluid" width="80px" height="80px" />
+                                                        <img src={equip.logo} className="img-fluid" width="100px" height="100px" />
                                                     </div>
                                                 </div>
                                             </td>
                                             <td>{equip.equipo}</td>
                                             <td>{equip.manager}</td>
                                             <td>
-                                                
-                                            </td>
-                                            <td> 
-                                                
-                                            
+                                                <div class="form-check">
+                                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                                                    <label className="form-check-label" for="flexCheckDefault"/>
+                                                </div>
                                             </td>
                                         </tr>
                                     )}
-
                                 </tbody>
                             </table>
                         </div>
@@ -112,23 +110,17 @@ export function Listadoequipos (){
                             type="button"
                             className="btn btn-secondary"
                             data-bs-dismiss="modal"
-                        >
-                            Cancelar
-                        </button>
+                        >Cancelar</button>
                         <button
                             type="submit"
                             className="btn btn-primary"
-                        >
-                            Adicionar Equipos
-                        </button>
-    
-    
+                        >Adicionar Equipos</button>
                         </div>
                         </form>
                     </div>
                 </div>
             </div>
-            {/* <!-- FIN MODAL ADICIONAR CAMPEONATO --> */}
+            {/* <!-- FIN MODAL ADICIONAR EQUIPOS A CAMPEONATOS --> */}
         </>
         );
 }
