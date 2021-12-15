8cc5597a2b76be26edfa81882fb366fc04504932
leslysharyn <leslysharyncj@gmail.com>
2021-12-15 16:00:17 -0500

    conexión con Atlas

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
 
 const {jugador_rutas} = require("./rutas/jugador_rutas")
 const {campeonato_rutas} = require("./rutas/campeonato_rutas")
@@ -12,7 +13,7 @@ const {campeonato_rutas} = require("./rutas/campeonato_rutas")
 app.use("/players",jugador_rutas);
 app.use("/campeonatos", campeonato_rutas);
 
-mongoose.connect("mongodb://localhost:27017/alibdb")
+mongoose.connect(process.env.SERVER_DB_URL)
 .then(res=> console.log(res,"Conectado a la base de datos"))
 .catch(err=> console.log(err))
 
