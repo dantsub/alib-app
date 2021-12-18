2c8594ab104ce0ddf65dd5f496cecd85f37188a6
bioalejo <bioalejandrogomez@hotmail.com>
2021-12-18 11:36:23 -0500

    modificaciones en usuarios

--- backend/rutas/usuario_rutas.js
+++ backend/rutas/usuario_rutas.js
@@ -6,45 +6,19 @@ var fs = require("fs");
 var path = require("path");
 
 usuario_rutas.post("/guardar", function (req, res) {
-  const form = new formidable.IncomingForm();
-  const uploadFolder = path.join(__dirname, "assets");
-  // Basic Configuration
-  form.uploaddir = uploadFolder;
-
-  console.log(form);
-  form.parse(req, function (err, fields, files) {
+  const datos = req.body;
+  const usu = new usuariomodel(datos);
+  usu.save(function (err) {
     if (err) {
-      // Check for and handle any errors here.
-
-      console.error(err.message);
-      return;
-    }
-    console.log(files);
-    console.log(fields);
-    const nom = fields.nom;
-    const doc = fields.doc;
-    const email = fields.email;
-    const pass = fields.pass;
-    const idrol = fields.idrol;
-    const idestado = fields.idestado;
-    const dato = {
-      nom: nom,
-      doc: doc,
-      email: email,
-      pass: pass,
-      idrol: idrol,
-      idestado: idestado,
-    };
-    const us = new usuariosmodel(dato);
-    us.save(function (err) {
-      if (err) {
-        res.send({ status: "Error", msg: "El usuario no pudo ser guardados" });
-        return false;
-      }
       res.send({
-        status: "Ok",
-        msg: "El usuario fue guardado satisfactoriamente",
+        status: "Error",
+        msg: "Los usuarios no pudieron ser guardados",
       });
+      return false;
+    }
+    res.send({
+      status: "Ok",
+      msg: "Los usuarios fueron guardados satisfactoriamente",
     });
   });
 });
@@ -59,3 +33,5 @@ usuario_rutas.get("/listar", async function (req, res) {
       .send({ status: "Error", msg: "La base de datos está vacía", usuarios });
   } else res.status(200).send({ status: "Ok", msg: "Usuarios encontrados", usuarios });
 });
+
+exports.usuario_rutas = usuario_rutas;
--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,21 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 
-const {jugador_rutas} = require("./rutas/jugador_rutas")
-const {campeonato_rutas} = require("./rutas/campeonato_rutas")
-const {equipo_rutas} = require("./rutas/equipo_rutas")
+const { jugador_rutas } = require("./rutas/jugador_rutas");
+const { campeonato_rutas } = require("./rutas/campeonato_rutas");
+const { equipo_rutas } = require("./rutas/equipo_rutas");
+const { usuario_rutas } = require("./rutas/usuario_rutas");
 
-app.use("/players",jugador_rutas);
+app.use("/players", jugador_rutas);
 app.use("/campeonatos", campeonato_rutas);
 app.use("/equipos", equipo_rutas);
+app.use("/usuarios", usuario_rutas);
 
-mongoose.connect(process.env.SERVER_DB_URL)
-.then(res=> console.log(res,"Conectado a la base de datos"))
-.catch(err=> console.log(err))
+mongoose
+  .connect(process.env.SERVER_DB_URL)
+  .then((res) => console.log(res, "Conectado a la base de datos"))
+  .catch((err) => console.log(err));
 
-
-app.listen(8081, function(){
-    console.log("servidor por el puerto 8081")
+app.listen(8081, function () {
+  console.log("servidor por el puerto 8081");
 });
