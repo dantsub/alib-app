42f072483a7b5902569b880eaf517236e476eeef
bioalejo <bioalejandrogomez@hotmail.com>
2021-12-21 13:46:48 -0500

    modelos

--- backend/modelos/partidosmodel.js
+++ backend/modelos/partidosmodel.js
@@ -1,47 +1,41 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 const partidoschema = new Schema({
-    estado:{
-        type:"string",
-        required:true
-    },
-    local:{
-        type:Schema.Types.ObjectId,
-        required:true,
-        ref:"equipos"
-
-    },
-    
-    visitante:{
-        type:Schema.Types.ObjectId,
-        required:true,
-        ref:"equipos"
-    },
-    rlocal:{
-        type:"number",
-        required:true
-    },
-    rvisitante:{
-        type:"number",
-        required:true
-    },
-   
-    fecha:{
-        type:"date",
-        required:true
-    }
-    
+  estado: {
+    type: "string",
+    required: true,
+  },
+  local: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "equipos",
+  },
+  visitante: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "equipos",
+  },
+  rlocal: {
+    type: "number",
+    required: true,
+  },
+  rvisitante: {
+    type: "number",
+    required: true,
+  },
+  fecha: {
+    type: "date",
+    required: true,
+  },
 });
 
-
-partidoschema.virtual("partidos",{
-    ref : "partidos", //la collection de datos con las que se relaciona
-    localField: "_id", //
-    foreignField: "cpartido" 
+partidoschema.virtual("partidos", {
+  ref: "partidos", //la collection de datos con las que se relaciona
+  localField: "_id", //
+  foreignField: "cpartido",
 });
 
-partidoschema.set("toObject", { virtuals : true });
-partidoschema.set("toJSON", { virtuals : true})
+partidoschema.set("toObject", { virtuals: true });
+partidoschema.set("toJSON", { virtuals: true });
 
-
-const partidomodel= model("partidos", partidoschema);
-exports.partidosmodel=partidomodel;
+const partidomodel = model("partidos", partidoschema);
+exports.partidosmodel = partidomodel;
--- backend/modelos/posicionesmodel.js
+++ backend/modelos/posicionesmodel.js
@@ -1,51 +1,23 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 const posicioneschema = new Schema({
-    nombre:{
-        type:"string",
-        required:true
-    },
-    PG:{
-        type:"number",
-        required:true
-    },
-    PP:{
-        type:"number",
-        required:true
-    },
-    PE:{
-        type:"number",
-        required:true
-    },    
-    GF:{
-        type:"number",
-        required:true
-    },    
-    GC:{
-        type:"number",
-        required:true
-    },
-
-    DIF:{
-        type:"number",
-        required:true
-    },
-
-    PUNTOS:{
-        type:"number",
-        required:true
-    }
+  equipo: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "equipos",
+  },
+  puntos: {
+    type: "number",
+    required: true,
+  },
+  poscamp: {
+    type: Schema.Types.ObjectId,
+    ref: "campeonatos", //nombre de la collection a la que te vas a relacionar
+    required: true,
+  },
 });
 
+posicioneschema.set("toObject", { virtuals: true });
+posicioneschema.set("toJSON", { virtuals: true });
 
-posicioneschema.virtual("posiciones",{
-    ref : "posiciones", //la collection de datos con las que se relaciona
-    localField: "_id", //
-    foreignField: "cposicion" 
-});
-
-posicioneschema.set("toObject", { virtuals : true });
-posicioneschema.set("toJSON", { virtuals : true})
-
-
-const posicionmodel= model("posiciones", posicioneschema);
-exports.posicionesmodel=posicionmodel;
+const posicionmodel = model("posiciones", posicioneschema);
+exports.posicionesmodel = posicionmodel;
