e79090246c97d991f4e5d3e9bf6eb11900bc9f63
nldams <nldams@gmail.com>
2021-12-20 23:29:02 -0500

    Eqipo_aprobar

--- backend/rutas/equipo_rutas.js
+++ backend/rutas/equipo_rutas.js
@@ -115,4 +115,36 @@ equipo_rutas.post("/inscribir", function(req,res){
     })
 });
 
+
+
+equipo_rutas.post("/aprobarequip", function(req,res){
+    const datos = req.body;
+    const eusuario= datos.eusuario;
+    const estadocamp = datos.estadocamp;
+    const filter = { eusuario: eusuario };
+    const updateDoc = {
+        $set: {
+            estadocamp: estadocamp
+        },
+      };
+    equipomodel.updateOne(filter, updateDoc, function(error,eq){
+        console.log(eq);
+        if (error){
+            res.send({status:"Error",msg:"El equipo NO fue aprobado por Error"});
+            return false;
+        }
+        else{
+        if (eq){
+            res.send({status:"Ok",msg:"El equipo fue aprobado satisfactoriamente", dato:eq})
+        }
+        else{
+            res.send({status:"Error",msg:"El equipo No pudo ser aprobado, porque no fue encontrado"})
+        }
+    }
+
+    })
+});
+
+
+
 exports.equipo_rutas=equipo_rutas;
