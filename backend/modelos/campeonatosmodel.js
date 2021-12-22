4a245d81bbcefeb47d156da598716b850b0901c4
bioalejo <bioalejandrogomez@hotmail.com>
2021-12-21 21:30:54 -0500

    cambios registrar posicion entre otros

--- backend/modelos/campeonatosmodel.js
+++ backend/modelos/campeonatosmodel.js
@@ -1,63 +1,67 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 const campeonatoschema = new Schema({
-    nombrecamp:{
-        type:"string",
-        required:true
-    },
-    fecinicamp:{
-        type:"date",
-        required:true
-    },
-    fecfincamp:{
-        type:"date",
-        required:true
-    },
-    orgcamp:{
-        type:"string",
-        required:true
-    },    
-    lugarcamp:{
-        type:"string",
-        required:true
-    },    
-    numequipcamp:{
-        type:"number",
-        required:true
-    },
-    premioscamp:{
-        type:"string",
-        required:true
-    },
-    logocamp:{
-        type:"string"
-    },
-    estadocamp:{
-        type:"string"
-    }         
+  nombrecamp: {
+    type: "string",
+    required: true,
+  },
+  fecinicamp: {
+    type: "date",
+    required: true,
+  },
+  fecfincamp: {
+    type: "date",
+    required: true,
+  },
+  orgcamp: {
+    type: "string",
+    required: true,
+  },
+  lugarcamp: {
+    type: "string",
+    required: true,
+  },
+  numequipcamp: {
+    type: "number",
+    required: true,
+  },
+  premioscamp: {
+    type: "string",
+    required: true,
+  },
+  logocamp: {
+    type: "string",
+  },
+  estadocamp: {
+    type: "string",
+  },
 });
 
-campeonatoschema.virtual("fechascampeonatos",{
-    ref : "fechas", //la collection de datos con las que se relaciona
-    localField: "_id", //
-    foreignField: "ecamp" //el nombre del campo en jugadormodel
+campeonatoschema.virtual("fechascampeonatos", {
+  ref: "fechas", //la collection de datos con las que se relaciona
+  localField: "_id", //
+  foreignField: "ecamp", //el nombre del campo en jugadormodel
 });
 
-
-campeonatoschema.virtual("integrantescampeonato",{
-    ref : "equipos", //la collection de datos con las que se relaciona
-    localField: "_id", //
-    foreignField: "ecamp" //el nombre del campo en jugadormodel
+campeonatoschema.virtual("integrantescampeonato", {
+  ref: "equipos", //la collection de datos con las que se relaciona
+  localField: "_id", //
+  foreignField: "ecamp", //el nombre del campo en jugadormodel
 });
 
-campeonatoschema.virtual("aprobacioncampeonato",{
-    ref : "equipos", //la collection de datos con las que se relaciona
-    localField: "_id", //
-    foreignField: "estadocamp" //el nombre del campo en jugadormodel
+campeonatoschema.virtual("aprobacioncampeonato", {
+  ref: "equipos", //la collection de datos con las que se relaciona
+  localField: "_id", //
+  foreignField: "estadocamp", //el nombre del campo en jugadormodel
 });
 
-campeonatoschema.set("toObject", { virtuals : true });
-campeonatoschema.set("toJSON", { virtuals : true})
+campeonatoschema.virtual("posicioncampeonato", {
+  ref: "posiciones", //la collection de datos con las que se relaciona
+  localField: "_id", //
+  foreignField: "poscamp", //el nombre del campo en jugadormodel
+});
 
+campeonatoschema.set("toObject", { virtuals: true });
+campeonatoschema.set("toJSON", { virtuals: true });
 
-const campeonatomodel= model("campeonatos", campeonatoschema);
-exports.campeonatomodel=campeonatomodel;
+const campeonatomodel = model("campeonatos", campeonatoschema);
+exports.campeonatomodel = campeonatomodel;
